35755409e7501fbd357ed1b6455d6efeea1eae55
Elnur <bairamovei@gmail.com>
2019-01-30 15:39:56 -0500

    Private Channel Creation Test

--- src/test/java/com/brite_erp/utilities/ApplicationConstants.java
+++ src/test/java/com/brite_erp/utilities/ApplicationConstants.java
@@ -2,4 +2,6 @@ package com.brite_erp.utilities;
 
 public class ApplicationConstants {
     public static final String APPLICATION_TITLE = "Odoo";
+
+    public  static  final String NEW_CHANNEL = "new";
 }
